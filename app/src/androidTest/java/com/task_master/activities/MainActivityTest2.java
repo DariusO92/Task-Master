8c5218dd5284a7df8750b5db3c5fceb07f96d651
Darius <Darius.owens.j@gmail.com>
2022-09-26 21:55:47 -0700

    added test

---
+++ app/src/androidTest/java/com/task_master/activities/MainActivityTest2.java
@@ -0,0 +1,55 @@
+package com.task_master.activities;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static org.hamcrest.Matchers.allOf;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import com.task_master.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest2 {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest2() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction imageButton = onView(
+                allOf(withId(R.id.imageButtonid),
+                        withParent(allOf(withId(R.id.GoToAppSettingsButtonID),
+                                withParent(withId(android.R.id.content)))),
+                        isDisplayed()));
+        imageButton.check(matches(isDisplayed()));
+
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.TaskRecyclerViewID),
+                        withParent(allOf(withId(R.id.GoToAppSettingsButtonID),
+                                withParent(withId(android.R.id.content)))),
+                        isDisplayed()));
+        recyclerView.check(matches(isDisplayed()));
+    }
+}
