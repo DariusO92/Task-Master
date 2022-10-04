b25e38785ef7d7b3ebe43b191047dde9c0a9f2a5
Darius <Darius.owens.j@gmail.com>
2022-10-03 20:49:26 -0700

    added signup, login, and verify Activity classes

---
+++ app/src/main/java/com/task_master/activities/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.task_master.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.task_master.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
